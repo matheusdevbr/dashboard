cf08cbda17c074d32f28ac81bcf7228ba8af2f3d
Matheus Santiago <matheussantiagopmw@gmail.com>
2021-08-28 13:40:00 -0300

    melhorando codigo duplicado com Input

---
+++ src/app/consulta/consulta-por-especialidade/consulta-por-especialidade.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ConsultaPorEspecialidadeComponent } from './consulta-por-especialidade.component';
+
+describe('ConsultaPorEspecialidadeComponent', () => {
+  let component: ConsultaPorEspecialidadeComponent;
+  let fixture: ComponentFixture<ConsultaPorEspecialidadeComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ConsultaPorEspecialidadeComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ConsultaPorEspecialidadeComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
