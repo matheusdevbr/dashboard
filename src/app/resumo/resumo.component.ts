8c302a60e15e156767ea83d7aa4bc76b656985c6
Matheus Santiago <matheussantiagopmw@gmail.com>
2021-08-28 09:36:32 -0300

    add página de resumo

--- src/app/resumo/resumo.component.ts
+++ src/app/resumo/resumo.component.ts
@@ -7,6 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResumoComponent implements OnInit {
 
+  resumo = {
+    
+      consultas : {
+        consultas_30dias_anteriores : 87,
+        consultas_30dias_posteriores : 79
+      },
+      faturamento : {
+        anterior : {
+          valor : 100000,
+          comparativo : 19
+        },
+        previsao : {
+          valor : 90000,
+          comparativo : -10	
+        }
+      }
+  }
+  
   constructor() { }
 
   ngOnInit(): void {
